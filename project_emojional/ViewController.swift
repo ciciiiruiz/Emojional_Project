6356a04c33458e9ed571320c63116e02f82cbec9
Citlally Ruiz <ciciiiruiz@gmail.com>
2019-06-20 14:41:17 -0500

    Initial Commit

---
+++ project_emojional/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project_emojional
+//
+//  Created by Apple on 6/20/19.
+//  Copyright © 2019 Citlally Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
