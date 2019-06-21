889d96b2404c93c5d584477b93d4793ca7132a29
Citlally Ruiz <ciciiiruiz@gmail.com>
2019-06-21 10:25:00 -0500

    DComplete IterIteration 3

--- project_emojional/ViewController.swift
+++ project_emojional/ViewController.swift
@@ -15,21 +15,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    var emojis = ["🌞": "Sunny & Happy!", "🌚": "Moony & Gloomy."]
+    let emojis = ["🌞": "Sunny & Happy!", "🌚": "Moony & Gloomy."]
+    
+    let customMessages = ["Sunny & Happy!": ["We all want to be happy, right?", "After all, life's better when we're happy, healthy and successful"],
+        "Moony & Gloomy.": ["Once you choose hope, anything is possible.", "Maybe you have to know the darkness before you can appreciate the light"]]
+    
     
     @IBAction func showMessage(sender:
         UIButton) {
-        let selesctedSunEmoji = sender.titleLabel?.text
+        let selectedEmoji = sender.titleLabel!.text!
         
-        let alertController = UIAlertController(title: "In a SUNNY mood ☼", message: "We all want to be happy, right?", preferredStyle: UIAlertController.Style.alert)
+        let num = Int.random(in: 0 ... 1)
+        
+        let emojiMessage = customMessages[emojis[selectedEmoji]!]?[num]
+        
+//        let randomMessage = customMessages.randomElement()
+        
+        let alertController = UIAlertController(title: "☼ ☾", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:
         nil))
         
-            print(emojis["🌞"])
+//            print(emojiMessage)
         
         present(alertController, animated: true, completion: nil)
     }
     
-    let customMessages = ["Sunny & Happy!": ["]]
+    
     
 }
